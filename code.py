11a95a642905e385e171db9030b04b7ab33a321f
Acetech <akashsolanke22@gmail.com>
2023-07-29 00:48:17 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,23 +1,11 @@
 import streamlit as st
-from snowflake.connector import connect
-from snowflake.connector.connection import SnowflakeConnection
-import toml
-import pandas as pd
 
+# Initialize connection.
+conn = st.experimental_connection('snowpark')
 
-@st.cache_resource()
-def get_connector() -> SnowflakeConnection:
-    """Create a connector to SnowFlake using credentials filled in Streamlit secrets"""
-    connector = connect(**st.secrets["snowflake"], client_session_keep_alive=True)
-    return connector
+# Perform query.
+df = conn.query('SELECT * from mytable;', ttl=600)
 
-# Time to live: the maximum number of seconds to keep an entry in the cache
-TTL = 24 * 60 * 60
-
-# Using `experimental_memo()` to memoize function executions
-@st.cache_data(ttl=TTL)
-def get_databases(_connector) -> pd.DataFrame:
-    """Get all databases available in Snowflake"""
-    return pd.read_sql("SHOW DATABASES;", _connector)
-
-
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.NAME} has a :{row.PET}:")
