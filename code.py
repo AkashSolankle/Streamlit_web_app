12e93a023a6e751ef82db9f5985ec491d4e9229a
Acetech <akashsolanke22@gmail.com>
2023-07-28 19:29:07 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,12 @@
-import pandas as pd
 import streamlit as st
+from snowflake.snowpark import Session
+from secrets import SNOWFLAKE_ACCOUNT, SNOWFLAKE_USER, SNOWFLAKE_PASSWORD
+
+
+session = Session(url=SNOWFLAKE_ACCOUNT, user=SNOWFLAKE_USER, password=SNOWFLAKE_PASSWORD)
+df = session.sql("SELECT * FROM PETS.MYTABLE").to_pandas()
+st.write(df)
+
 
 st.title("Hi Anber! Welcome to Streamlit!")
 
