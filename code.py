b9efc76c8dbf0b8287205fb4c6830e4749885740
Acetech <akashsolanke22@gmail.com>
2023-07-29 02:19:38 +0530

    Update code.py

--- code.py
+++ code.py
@@ -3,23 +3,23 @@ import streamlit as st
 # Initialize connection.
 conn = st.experimental_connection('snowpark')
 #status elements
-#st.snow()
-#st.balloons()
+  #st.snow()
+  #st.balloons()
 #Title
-st.title('First :blue[Streamlit] web app :sunglasses:')
+  st.title('First :blue[Streamlit] web app :sunglasses:')
 
 #Code block
-code = '''st.title('First :blue[Streamlit] web app :sunglasses:')'''
-st.code(code, language='python')
+  #code = '''st.title('First :blue[Streamlit] web app :sunglasses:')'''
+  #st.code(code, language='python')
 
 #with st.chat_message("user"):
 #    st.write("Hello 👋")
 # Perform query.
-df = conn.query('SELECT * from MYTABLE;', ttl=600)
+  df = conn.query('SELECT * from MYTABLE;', ttl=600)
 
 # Print results.
-#for row in df.itertuples():
-#    st.write(f"{row.NAME}:{row.PET}:")
-
-st.line_chart(df)
-st.area_chart(df)
+  #for row in df.itertuples():
+  #    st.write(f"{row.NAME}:{row.PET}:")
+#Charts
+  st.line_chart(df)
+  st.area_chart(df)
