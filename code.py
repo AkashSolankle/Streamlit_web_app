5be32cf94d77777ece0f6c61e965205ff05d1c46
Acetech <akashsolanke22@gmail.com>
2023-07-28 19:34:39 +0530

    Update code.py

--- code.py
+++ code.py
@@ -3,18 +3,7 @@ from snowflake.snowpark import Session
 from secrets import SNOWFLAKE_ACCOUNT, SNOWFLAKE_USER, SNOWFLAKE_PASSWORD
 
 
-session = Session(url=SNOWFLAKE_ACCOUNT, user=SNOWFLAKE_USER, password=SNOWFLAKE_PASSWORD)
+session = Session(url=st.secrets["SNOWFLAKE_ACCOUNT"], user=st.secrets["SNOWFLAKE_USER"], password=st.secrets["SNOWFLAKE_PASSWORD"])
+
 df = session.sql("SELECT * FROM PETS.MYTABLE").to_pandas()
 st.write(df)
-
-
-st.title("Hi Anber! Welcome to Streamlit!")
-
-st.write("My first DataFrame")
-
-st.write(
-pd.DataFrame({
-    'A': [1, 5, 9, 7],
-    'B': [3, 2, 4, 8]
-  })
-)
