a8a2f7b6c669105da0ac3e92d46bd76dbd34c64e
Acetech <akashsolanke22@gmail.com>
2023-07-29 01:50:58 +0530

    Update code.py

--- code.py
+++ code.py
@@ -4,10 +4,10 @@ import streamlit as st
 conn = st.experimental_connection('snowpark')
 
 # Perform query.
-df = conn.query('SELECT * from MYTABLE;', ttl=600)
+df = conn.query('SELECT * from KORIA_WEATHER_REPORT;', ttl=600)
 
 # Print results.
-for row in df.itertuples():
-    st.write(f"{row.NAME}:{row.PET}:")
+#for row in df.itertuples():
+#    st.write(f"{row.NAME}:{row.PET}:")
 
 st.line_chart(df)
