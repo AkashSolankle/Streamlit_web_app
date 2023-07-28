acd2fb976d1ac58d104f9ef5bed9841011255ef7
Acetech <akashsolanke22@gmail.com>
2023-07-28 21:07:56 +0530

    Update code.py

--- code.py
+++ code.py
@@ -4,7 +4,7 @@ from snowflake.connector.connection import SnowflakeConnection
 import toml
 
 
-@st.experimental_singleton()
+@st.cache_resource()
 def get_connector() -> SnowflakeConnection:
     """Create a connector to SnowFlake using credentials filled in Streamlit secrets"""
     connector = connect(**st.secrets["snowflake"], client_session_keep_alive=True)
