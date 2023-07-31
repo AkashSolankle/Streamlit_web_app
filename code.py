a3781857754c0a7e03f888b81544160e7318c643
Acetech <akashsolanke22@gmail.com>
2023-07-31 19:27:04 +0530

    Update code.py

--- code.py
+++ code.py
@@ -10,7 +10,7 @@ import streamlit as st
 dataset_url = "https://raw.githubusercontent.com/Lexie88rus/bank-marketing-analysis/master/bank.csv"
 #summary of data
 # read csv from a URL
-@st.experimental_memo
+@st.cache_data
 def get_data() -> pd.DataFrame:
     return pd.read_csv(dataset_url)
 
