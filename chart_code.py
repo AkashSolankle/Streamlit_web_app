ee6758d82ba22cd709b28e5db812bd31776e13a2
Acetech <akashsolanke22@gmail.com>
2023-07-31 19:24:48 +0530

    Create chart_code.py

---
+++ chart_code.py
@@ -0,0 +1,136 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.figure_factory as ff
+import plotly.express as px
+import pydeck as pdk
+import scipy
+import matplotlib.pyplot as plt
+
+
+
+#read csv file
+movies_data = pd.read_csv("https://raw.githubusercontent.com/danielgrijalva/movie-stats/7c6a562377ab5c91bb80c405be50a0494ae8e582/movies.csv")
+#summary of data
+movies_data.info()
+
+st.set_page_config(page_title='Covid App', page_icon=':mask:')
+# Initialize connection.
+conn = st.experimental_connection('snowpark')
+#status elements
+  #st.snow()
+  #st.balloons()
+#Title
+st.title('First :blue[Streamlit] web app :sunglasses:')
+st.text("")
+st.text("")
+
+st.sidebar.success('Welcome to Home Page :tada:')
+#Code block
+  #code = '''st.title('First :blue[Streamlit] web app :sunglasses:')'''
+  #st.code(code, language='python')
+
+#with st.chat_message("user"):
+#    st.write("Hello 👋")
+# Perform query.
+df = conn.query('SELECT * from KORIA_WEATHER_REPORT;', ttl=600)
+
+# Print results.
+  #for row in df.itertuples():
+  #    st.write(f"{row.NAME}:{row.PET}:")
+#Charts
+#st.line_chart(df)
+#st.area_chart(df)
+
+#1st chart
+# Add histogram data
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
+
+# Group data together
+hist_data = [x1, x2, x3]
+
+group_labels = ['Group 1', 'Group 2', 'Group 3']
+
+# Create distplot with custom bin_size
+fig = ff.create_distplot(
+        hist_data, group_labels, bin_size=[.1, .25, .5])
+
+# Plot!
+st.plotly_chart(fig, use_container_width=True)
+
+
+# 2nd graph
+df = px.data.gapminder()
+fig = px.scatter(
+    df.query("year==2007"),
+    x="gdpPercap",
+    y="lifeExp",
+    size="pop",
+    color="continent",
+    hover_name="country",
+    log_x=True,
+    size_max=60,
+)
+
+tab1, tab2 = st.tabs(["Streamlit theme (default)", "Plotly native theme"])
+with tab1:
+    # Use the Streamlit theme.
+    # This is the default. So you can also omit the theme argument.
+    st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+with tab2:
+    # Use the native Plotly theme.
+    st.plotly_chart(fig, theme=None, use_container_width=True)
+
+
+#3rd chart
+st.subheader("Define a custom colorscale")
+df = px.data.iris()
+fig = px.scatter(
+    df,
+    x="sepal_width",
+    y="sepal_length",
+    color="sepal_length",
+    color_continuous_scale="reds",
+)
+
+tab1, tab2 = st.tabs(["Streamlit theme (default)", "Plotly native theme"])
+with tab1:
+    st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+with tab2:
+    st.plotly_chart(fig, theme=None, use_container_width=True)
+
+#chart4
+chart_data = pd.DataFrame(
+   np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+   columns=['lat', 'lon'])
+
+st.pydeck_chart(pdk.Deck(
+    map_style=None,
+    initial_view_state=pdk.ViewState(
+        latitude=37.76,
+        longitude=-122.4,
+        zoom=11,
+        pitch=50,
+    ),
+    layers=[
+        pdk.Layer(
+           'HexagonLayer',
+           data=chart_data,
+           get_position='[lon, lat]',
+           radius=200,
+           elevation_scale=4,
+           elevation_range=[0, 1000],
+           pickable=True,
+           extruded=True,
+        ),
+        pdk.Layer(
+            'ScatterplotLayer',
+            data=chart_data,
+            get_position='[lon, lat]',
+            get_color='[200, 30, 0, 160]',
+            get_radius=200,
+        ),
+    ],
+))
