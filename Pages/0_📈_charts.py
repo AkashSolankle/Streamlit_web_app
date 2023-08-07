23111bacc0ee657371e3002005cf87ece0b7e797
Acetech <akashsolanke22@gmail.com>
2023-08-08 01:18:18 +0530

    Update 0_📈_charts.py

--- Pages/0_📈_charts.py
+++ Pages/0_📈_charts.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
 
-st.set_page_config(layout="centered", page_title="Data Editor", page_icon="🧮")
+st.set_page_config(layout="centered", page_title="Covid App", page_icon="🧮")
 
 st.title("✍️ Charts")
