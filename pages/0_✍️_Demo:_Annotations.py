52ab75694816873a0c52c149cd7775a9de54ff4d
Acetech <akashsolanke22@gmail.com>
2023-08-08 01:26:15 +0530

    Update 0_✍️_Demo:_Annotations.py

--- pages/0_✍️_Demo:_Annotations.py
+++ pages/0_✍️_Demo:_Annotations.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import streamlit as st
 
-st.set_page_config(layout="centered", page_title="Data Editor", page_icon="🧮")
+st.set_page_config(layout="centered", page_title="Data Editor", page_icon=":mask:")
 
 st.title("✍️ Annotations")
 st.caption("This is a demo of the `st.data_editor`.")
