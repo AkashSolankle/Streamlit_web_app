b660e242aa17e7666372477c1820c023fdfa0ed9
Acetech <akashsolanke22@gmail.com>
2023-08-08 14:54:27 +0530

    Update code.py

--- code.py
+++ code.py
@@ -7,6 +7,7 @@ import pydeck as pdk
 import scipy
 import altair as alt
 import matplotlib.pyplot as plt
+import time
 from streamlit_extras.switch_page_button import switch_page
 from snowflake.snowpark.context import get_active_session
 
