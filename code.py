e968ffa9701b1d48c0e33ca1861bdad83b6fbd58
Acetech <akashsolanke22@gmail.com>
2023-08-08 12:37:07 +0530

    Update code.py

--- code.py
+++ code.py
@@ -7,9 +7,10 @@ import pydeck as pdk
 import scipy
 import matplotlib.pyplot as plt
 from streamlit_extras.switch_page_button import switch_page
+from snowflake.snowpark.context import get_active_session
 
 
-
+session = get_active_session()
 st.set_page_config(page_title='Covid App', page_icon=':mask:')
 # Initialize connection.
 conn = st.experimental_connection('snowpark')
@@ -31,7 +32,21 @@ st.sidebar.success('Welcome to Home Page :tada:')
 # Perform query.
 df = conn.query('SELECT top 1000 * from KORIA_WEATHER_REPORT;', ttl=600)
 #st.dataframe(df)
-st.data_editor(df)
+with st.form("data_editor_form"):
+    st.caption("Edit the dataframe below")
+    edited = st.data_editor(df, use_container_width=True, num_rows="dynamic")
+    submit_button = st.form_submit_button("Submit")
+
+if submit_button:
+    try:
+        #Note the quote_identifiers argument for case insensitivity
+        session.write_pandas(edited, "KORIA_WEATHER_REPORT", overwrite=True, quote_identifiers=False)
+        st.success("Table updated")
+        time.sleep(5)
+    except:
+        st.warning("Error updating table")
+    #display success message for 5 seconds and update the table to reflect what is in Snowflake
+    st.experimental_rerun()
 
 #st.line_chart(df)
 #st.area_chart(df)
