eddf4594f4b78e431d2052f64403d4c6bce9b551
Acetech <akashsolanke22@gmail.com>
2023-08-08 13:03:19 +0530

    Update code.py

--- code.py
+++ code.py
@@ -5,6 +5,7 @@ import plotly.figure_factory as ff
 import plotly.express as px
 import pydeck as pdk
 import scipy
+import altair as alt
 import matplotlib.pyplot as plt
 from streamlit_extras.switch_page_button import switch_page
 from snowflake.snowpark.context import get_active_session
