a6775a56b4ad06d4829d7bb91ff84592d6da759d
Acetech <akashsolanke22@gmail.com>
2023-09-07 19:05:58 +0530

    Update code.py

--- code.py
+++ code.py
@@ -28,7 +28,7 @@ st.sidebar.success('Welcome to Home Page :tada:')
   #code = '''st.title('First :blue[Streamlit] web app :sunglasses:')'''
   #st.code(code, language='python')
 user = session.sql( 'select current_user()')
-st.dataframe(user)
+st.dataframe(user.style.highlight_max(axis=0))
 #print(user.['CURRENT_USER()'].values[:1])
 with st.chat_message("user"):
     st.write(user)
